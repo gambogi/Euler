c68b3e82c12b5a11fa1eee673386bde266c70bb2
Matt Gambogi <m@gambogi.com>
2013-07-09 13:38:26 -0400

    Using binet's formula for nth fibonacci number

--- 2/pe2.hs
+++ 2/pe2.hs
@@ -1,5 +1,14 @@
 -- Find the sum of the even fibonacci numbers below 4 million
+
+--naive first approach
 fib n = fibs !! n
         where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)        
-main = do
-    print $ sum $ filter (even) $ takeWhile (<4000000) $ (map fib [1,2 ..])
+
+--Using Binet's formula
+--golden ratio
+f n = round $ phi ** fromIntegral n/sq5
+    where
+        sq5 = sqrt 5 :: Double
+        phi = (1+ sq5) / 2
+
+main = print $ sum $ filter (even) $ takeWhile (<4000000) $ (map f [1,2 ..])
