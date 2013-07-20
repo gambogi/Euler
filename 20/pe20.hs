f0c4872ae69bf9faeba8045888ccc4f717668a4d
Matt Gambogi <m@gambogi.com>
2013-07-20 17:20:22 -0400

    solved problem 20

---
+++ 20/pe20.hs
@@ -0,0 +1,7 @@
+-- find the sum of the digits in the number 100!
+import Data.Digits
+main = print $ sum $ digits 10 $ factorial 100
+
+factorial n
+    | n == 1     = n
+    | otherwise = n*(factorial $ n-1)
