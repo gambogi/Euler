c47b6438f70d1cea6fff35ae15c19966b3fdcf2a
Matt Gambogi <m@gambogi.com>
2013-07-29 09:53:49 -0400

    fixed definition of amicable number. Solved #21

--- 021/pe21.hs
+++ 021/pe21.hs
@@ -10,7 +10,7 @@ properDivisors n  = filter (divides n) [1..(n`div`2)]
     where divides a b = a`mod`b == 0
 
 amicable :: Int -> Bool
-amicable a = d (d a) == a
+amicable a = d (d a) == a  && a /= d a
     where d = sum . properDivisors
 
 main = print $ sum $ filter amicable [1..9999]
